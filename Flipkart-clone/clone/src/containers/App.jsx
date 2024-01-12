c22d11b91bb1f809ca05db142f3faff25c88ada0
Aman <jha04527@gmail.com>
2024-01-12 09:38:32 +0530

    Add React

--- Flipkart-clone/clone/src/containers/App.jsx
+++ Flipkart-clone/clone/src/containers/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React , {Suspense} from 'react'
+import {Routes , Route } from "react-router-dom"
+import Layout from '../Layouts/Layout'
+import AdminLayout from '../Layouts/AdminLayout'
+import { Home,UserProfile } from '../pages'
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-blue-600 font-semibold">App</div>
+    <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path ="/" element={<Home />} />
+            <Route path ="/profile/:uid" element={<UserProfile />} />
+
+            {/* admin layout*/}
+            <Route path="/admin" element={<AdminLayout />}>
+              
+            </Route>
+          </Route>
+        </Routes>
+    </Suspense>
   )
 }
 
